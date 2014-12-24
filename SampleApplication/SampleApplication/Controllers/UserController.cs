1b1192cb6d9b6ff6564a6b27fefbe84958fb6364
ISemyanchuk <isemyan@softserveinc.com>
2014-12-24 14:37:41 +0200

    test

---
+++ SampleApplication/SampleApplication/Controllers/UserController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleApplication.Models;
+
+namespace SampleApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        private SampleApplicationContext db = new SampleApplicationContext();
+
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View(db.UserModels.ToList());
+        }
+
+        //
+        // GET: /User/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            UserModel usermodel = db.UserModels.Find(id);
+            if (usermodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usermodel);
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create()
+        {
+            if (5 > 4)
+            {
+
+            }
+            return View();
+        }
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserModel usermodel)
+        {
+            if (ModelState.IsValid)
+            {
+               
+            }
+
+            return View(usermodel);
+        }
+
+        //
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UserModel usermodel = db.UserModels.Find(id);
+            if (usermodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usermodel);
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserModel usermodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usermodel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usermodel);
+        }
+
+        //
+        // GET: /User/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            UserModel usermodel = db.UserModels.Find(id);
+            if (usermodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usermodel);
+        }
+
+        //
+        // POST: /User/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserModel usermodel = db.UserModels.Find(id);
+            db.UserModels.Remove(usermodel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SampleApplication/SampleApplication/Models/UserModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+
+namespace SampleApplication.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Url]
+        public string   Url { get; set; }  
+
+        public DateTime DateOfBirth { get; set; }
+
+        [Range(0,int.MaxValue)]
+        public int JumpCount { get; set; }
+
+        [CreditCard]
+        public string CreditCard { get; set; }
+
+        public Color ParachuteColor { get; set; }
+
+        [DefaultValue(700)]
+        public double JumpHeight { get; set; }
+
+
+
+
+    }
+}
